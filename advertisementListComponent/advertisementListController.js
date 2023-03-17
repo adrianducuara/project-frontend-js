1849ff0f97640ef98183c3897d3f9f8833103f58
adrian.ducuara <adriandesarrollador@outlook.com>
2023-03-16 22:52:14 -0500

    feat(*): addeding new component and finalizing other

--- advertisementListComponent/advertisementListController.js
+++ advertisementListComponent/advertisementListController.js
@@ -1,34 +1,62 @@
 import { data } from "./advertisementListModel.js";
-import { buildAdvertisementList, buildLoading, buildWrapperLayout } from "./advertisementListView.js";
+import { buildAdvertisementList, buildLoading, buildModalError, buildWrapperLayout } from "./advertisementListView.js";
 
-export async function advertisementListViewController(element){
-    let wrapperAdvertisementList = buildWrapperLayout();
-    let loadingELement = buildLoading();
-    element.appendChild(wrapperAdvertisementList);
-    const productContainer = document.querySelector('#product-container');
-    let advertisementElement = '';
-    let dataAdvertisement = '';
+function messageEmptyAdvertisement(container) {
+    return container.textContent = 'No hay anuncios';
+}
+
+async function getDataAsync() {
+    let dataAdvertisement = {};
+
     try {
         dataAdvertisement = await data;
     } catch (error) {
-        console.log('Eror al cargar los anuncios', error);
+        dataAdvertisement = {};
     }
 
+    return dataAdvertisement;
+}
+
+function modalErrorService(modal) {
+    const modalButton = document.querySelector('#modal-button');
+
+    modal.classList.remove('hidden');
+
+    modalButton.addEventListener('click', () => {
+        modal.classList.add('hidden');
+    })
+}
+
+export async function advertisementListViewController(element){
+    let wrapperAdvertisementList = buildWrapperLayout();
+    let errorService = buildModalError('Hubo un problema', 'Se ha producido un error al cargar los productos.', 'Entendido');
+    let loadingELement = buildLoading();
+    element.appendChild(wrapperAdvertisementList);
+    element.appendChild(errorService);
+    const productContainer = document.querySelector('#product-container');
+    let advertisementElement = '';
+    
+    let dataAdvertisementList = await getDataAsync();
+    
     productContainer.appendChild(loadingELement);
 
-    if(dataAdvertisement.advertisements.length !== 0) {
-        productContainer.removeChild(loadingELement);
-        for (let advertisement of dataAdvertisement.advertisements) {
-            advertisementElement = buildAdvertisementList(advertisement);
-            productContainer.appendChild(advertisementElement);
-            
+    if(dataAdvertisementList.hasOwnProperty('advertisements')) {
+        if(dataAdvertisementList.advertisements.length !== 0) {
+            productContainer.removeChild(loadingELement);
+            for (let advertisement of dataAdvertisementList.advertisements) {
+                advertisementElement = buildAdvertisementList(advertisement);
+                productContainer.appendChild(advertisementElement);
+            }
+        }else {
+            productContainer.removeChild(loadingELement);
+            messageEmptyAdvertisement(productContainer);
         }
-    } else {
-        productContainer.textContent = 'No hay anuncios';        
+    }else {
+        messageEmptyAdvertisement(productContainer);
+        modalErrorService(errorService);
     }
 
 
-    
 }
 
 
